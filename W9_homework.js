38b542effdea7b21cb312dede3db29e655363e83
Spenc <sger0007@student.monash.edu>
2023-09-24 16:00:26 +1000

    adjusting our html to support our vega lite map visualization as well as our js

---
+++ W9_homework.js
@@ -0,0 +1,4 @@
+var vg_1 = "W9_homework.vg.json";
+vegaEmbed("#map_idiom", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
